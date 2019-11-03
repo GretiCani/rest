fc93bce5b789d0b9bc6da6a4d4cac6e0c4126d8a
Greti Cani <greticani@ymail.com>
2019-11-03 19:02:05 +0100

    Implemented CRUD operation for User entity

--- src/main/java/com/rest/service/UserService.java
+++ src/main/java/com/rest/service/UserService.java
@@ -1,4 +1,44 @@
 package com.rest.service;
 
+import com.rest.model.User;
+import com.rest.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findUserById(UUID id){
+        return userRepository.findById(id);
+    }
+
+    public User updateUser(UUID id, User user){
+        user.setId(id);
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(UUID id){
+        if (userRepository.findById(id).isPresent()){
+            userRepository.deleteById(id);
+        }
+    }
+
+    public User findByUsername(String username){
+        return userRepository.findUserByUsername(username);
+    }
 }
